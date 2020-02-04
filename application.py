839a1aec726ee552737754084e2e3019ab39d91a
Muhammad Zeeshan Karamat <muhammadzeeshan020@gmail.com>
2020-02-04 21:50:02 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,5 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
-import subprocess
-import os
-import uuid
-import cv2
-import math
-from shutil import copyfile
-import json
-import Augmentor
-from distutils.dir_util import copy_tree
+
 
 app = Flask(__name__)
 
